4c90328adfeef536ae21c6639e55da07e8275f0b
James Smith <st23am@gmail.com>
2011-11-04 17:03:22 -0400

    added sandbox to precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module CskoansOnline
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
+    config.assets.precompile += ['sandbox.js']
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
